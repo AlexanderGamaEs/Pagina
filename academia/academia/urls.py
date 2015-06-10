4539bc953b4be29c9cecbf0a930170b75d3e7f6a
Alexander Gama <alexgama02@hotmail.com>
2015-06-10 16:03:31 -0500

    Agregados las urls

--- academia/academia/urls.py
+++ academia/academia/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'', include('main.urls'))
+    url(r'^main/', include('main.urls')), # ADD THIS NEW TUPLE!
+)
